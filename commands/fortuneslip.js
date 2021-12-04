9cb1b0509df70b2b5e0a4236d00a13fea4ffc2bc
shinyduck <74885976+shinyduck@users.noreply.github.com>
2021-12-04 14:36:47 +0800

    Update fortuneslip.js
    fixed multiple jobs being scheduled concurrently when the command is ran.

--- commands/fortuneslip.js
+++ commands/fortuneslip.js
@@ -4,22 +4,31 @@ const schedule = require('node-schedule');
 const { fortuneSlipEmbed } = require('../fortuneslipembeds');
 // Create new empty variable called 'claimeddailyfortuneslip'
 var claimeddailyfortuneslip = new Set();
-// var i = 0
+//var i = 0
 
+// schedule job reset At 04:00 (0 4 * * *) CRON
+schedule.scheduleJob('0 4 * * *', () => {
+    claimeddailyfortuneslip = new Set();
+    //i = (i + 1)
+    console.log('dailyfortuneslip reset', new Date().toString())
+    //console.log(i)
+});
+
+/*
+// schedule reset At every minute - for testing purposes (* * * * *) CRON
+schedule.scheduleJob('* * * * *', () => {
+    claimeddailyfortuneslip = new Set();
+    //i = (i + 1)
+    console.log('dailyfortuneslip reset', new Date().toString())
+    //console.log(i)
+});
+*/
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('fortuneslip')
     .setDescription('claim your daily fortune slip. (resets at 4AM)'),
     async execute(interaction) {
-        // schedule job reset At 04:00 (0 4 * * *) CRON
-        const job = schedule.scheduleJob('0 4 * * *', () => {
-            claimeddailyfortuneslip = new Set();
-            // i = (i + 1)
-            console.log('dailyfortuneslip reset', new Date().toString())
-            // console.log(i)
-        });
-
         if (claimeddailyfortuneslip.has(interaction.user.id)) {
             interaction.reply({
                 content: `You've already gotten one today. Please try again tomorrow... ${interaction.user} (resets at 4AM)`,
